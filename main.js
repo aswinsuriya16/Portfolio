2280c712eb97fe79b1756a7b9d9a6d60d0e50421
aswinsuriya16 <aswinsuriya16@gmail.com>
2025-01-15 18:36:41 +0530

    made change

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:["Web Developer","FreeLancer","Problem Solver"],
+    strings:["Web Developer","FreeLancer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
